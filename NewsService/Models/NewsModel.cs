696a697a80e72d63a6599b4db15f190645d12465
patrick <patrick@PATRICKN-OLSPS>
2021-06-20 14:05:01 +0200

    Add project files.

---
+++ NewsService/Models/NewsModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NewsService.Models
+{
+    public class NewsModel
+    {
+            
+            public string Headline { get; set; }
+            public string Text { get; set; }
+            public int RelevanceScore { get; set; }
+            public DateTime Date { get; set; }
+            public string Source { get; set; }
+            public string[] Tags { get; set; }
+        
+
+    }
+
+}
+
+
